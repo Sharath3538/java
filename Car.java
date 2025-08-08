a42b9cfd647eed4604df0438d34efe606c3049ef
Sharath3538 <Sharath3538>
2025-08-08 16:59:04 +0530

    here wonderful

---
+++ Car.java
@@ -0,0 +1,24 @@
+ class Car{
+	 public static void main(String [] args){
+		 
+ byte numberOfDoors=4;
+ short engineCC=1500;
+ int topSpeed=220;
+ long registrationNumber=9876543210L;
+ float fuelCapacity=45.51f;
+ double mileage=18.75;
+ char fuelType='P';
+ boolean isAutomatic=true;
+
+System.out.println("numberOfDoors="+numberOfDoors);
+System.out.println("engineCC="+engineCC);
+System.out.println("topSpeed="+topSpeed);
+System.out.println("registrationNumber="+registrationNumber);
+System.out.println("fuelCapacity="+fuelCapacity);
+System.out.println("mileage="+mileage);
+System.out.println("fuelType="+fuelType);
+System.out.println("isAutomatic="+isAutomatic);
+ }
+ 
+ }
+ 

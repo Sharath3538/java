d29db18de925ca6a2b0e7e7f720c2f607a3a4e8d
Sharath3538 <Sharath3538>
2025-09-01 18:54:15 +0530

    staticvariables

---
+++ AmericaRunner.java
@@ -0,0 +1,45 @@
+class AmericaRunner{
+public static void main (String [] args){
+double people=America.Population(1);
+System.out.println("Alabama="+people);
+ people=America.Population(2);
+System.out.println("Alaska="+people);
+ people=America.Population(3);
+System.out.println("Arizona="+people);
+ people=America.Population(4);
+System.out.println("Arkansas="+people);
+ people=America.Population(5);
+System.out.println("California="+people);
+ people=America.Population(6);
+System.out.println("Colorado="+people);
+ people=America.Population(7);
+System.out.println("Connecticut="+people);
+ people=America.Population(8);
+System.out.println("Delaware="+people);
+ people=America.Population(9);
+System.out.println("Florida="+people);
+ people=America.Population(10);
+System.out.println("Georgia="+people);
+ people=America.Population(11);
+System.out.println("Hawaii="+people);
+ people=America.Population(12);
+System.out.println("Idaho="+people);
+ people=America.Population(13);
+System.out.println("Illinois="+people);
+ people=America.Population(14);
+System.out.println("Indiana="+people);
+ people=America.Population(15);
+System.out.println("Iowa="+people);
+ people=America.Population(16);
+System.out.println("Kansas="+people);
+ people=America.Population(17);
+System.out.println("Kentucky="+people);
+ people=America.Population(18);
+System.out.println("Louisiana="+people);
+ people=America.Population(19);
+System.out.println("Maine="+people);
+ people=America.Population(20);
+System.out.println("Maryland="+people);
+
+}
+}

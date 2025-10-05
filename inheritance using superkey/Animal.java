9816e2b2f63f3772b1517850023ddee3c506aa45
Sharath <sharuxworkz@gmail.com>
2025-10-05 22:37:08 +0530

    inheritance using superkey

--- inheritance using superkey/Animal.java
+++ inheritance using superkey/Animal.java
@@ -5,8 +5,10 @@ public long animaldoctorNum;
 public double animalfoodRate;
 public char animalfirstWord;
 public float animallovePer;
-public boolean animalcaring;
-public Animal(String animalName,int animalmatPrice,long animaldoctorNum,double animalfoodRate,char animalfirstWord,float animallovePer,boolean animalcaring){
+public boolean animalCaring;
+public Animal(String animalName,int animalmatPrice,long animaldoctorNum,double animalfoodRate,char animalfirstWord,float animallovePer,boolean animalCaring){
+
+	
 super();
 this.animalName=animalName;
 this.animalmatPrice=animalmatPrice; 
@@ -14,7 +16,7 @@ this.animaldoctorNum=animaldoctorNum;
 this.animalfoodRate=animalfoodRate;
 this.animalfirstWord=animalfirstWord;
 this.animallovePer=animallovePer;
-this.animalcaring=animalcaring;
+this.animalCaring=animalCaring;
 
 }
 public void displayAnimalDetails(){
@@ -24,13 +26,13 @@ System.out.println("animal doctor number is="+this.animaldoctorNum);
 System.out.println("animal food rate is="+this.animalfoodRate);
 System.out.println("animal first word is="+this.animalfirstWord);
 System.out.println("animal love percentage is="+this.animallovePer);
-System.out.println("animal caring is real are="+this.animalcaring);
+System.out.println("animal caring is real are="+this.animalCaring);
 System.out.println("Dog Details"+super.animalName);
 System.out.println("Dog Details"+super.animalmatPrice);
 System.out.println("Dog Details"+super.animaldoctorNum);
 System.out.println("Dog Details"+super.animalfoodRate);
 System.out.println("Dog Details"+super.animalfirstWord);
 System.out.println("Dog Details"+super.animallovePer);
-System.out.println("Dog Details"+super.animalcaring);
+System.out.println("Dog Details"+super.animalCaring);
 }
 }
